6c076d4cc877449aa1c7214de068b141c56bd95e
Francisco Machado e Costa <frmachadoecosta>
2019-03-09 11:29:38 +0000

    Updated fetch method
    Less missing values and lines of code

--- goodreads.py
+++ goodreads.py
@@ -2,7 +2,9 @@
 This small script serves to choose what books should I read from Horus Heresy, a series of over 50 books
 '''
 
-#'OTJH4NwxE8fITsx89D9OA', 'w3mliUQHNxV91MjRIQ8gtIK5aTaW1GPKJBNfSU9b2Po')
+#'OTJH4NwxE8fITsx89D9OA', 'w3mliUQHNxV91MjRIQ8gtIK5aTaW1GPKJBNfSU9b2Po'
+#throwaway key and secret
+
 import goodreads_api_client as gr
 import pandas as pd
 
@@ -15,22 +17,28 @@ columns = ['title','author','publication_year','average_rating','ratings_count']
 wh40kdf = pd.DataFrame(columns=columns)
 
 
+
+def nadiv(p1,p2=1):
+    if p1 is None or p2 is None:
+        return ''
+
+    p1,p2 = int(p1), int(p2)
+    return round(p1/p2,2)
+
+
+
 for bookdict in serie['series_works']['series_work']:
+
     info = bookdict['work']
     info2 = info['best_book']
 
-    try:
-        print('entrei')
-        wh40kdf= wh40kdf.append({'title': info2['title'],
-                        'author': info2['author']['name'],
-                        'publication_year':int(info['original_publication_year']),
-                        'average_rating': int(info['ratings_sum'])/int(info['ratings_count']),
-                        'ratings_count': int(info['ratings_count'])},ignore_index=True)
-    except TypeError:
-        wh40kdf = wh40kdf.append({'title': info2['title'],
-                        'author': info2['author']['name'],
-                        'publication_year': None,
-                        'average_rating': None,
-                        'ratings_count': None},ignore_index=True)
+    wh40kdf= wh40kdf.append({
+                    'title': info2['title'],
+                    'author': info2['author']['name'],
+                    'publication_year':nadiv(info['original_publication_year']),
+                    'average_rating': nadiv(info['ratings_sum'],info['ratings_count']),
+                    'ratings_count': nadiv(info['ratings_count'])
+                },ignore_index=True)
+
 
 wh40kdf.to_csv('Warhammer_Books.csv')
